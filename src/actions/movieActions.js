dc8e42a28c9c6b68a3fe50e896df80d66232b256
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-10 15:15:42 -0400

    need to fix header file

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -1,5 +1,18 @@
 export const DELETE_MOVIE = "DELETE_MOVIE";
+export const ADD_MOVIE = "ADD_MOVIE";
+export const TOGGLE_FAVORITE = "TOGGLE_FAVORITE";
 
-export const deleteMovie = (id)=>{
-    return({type: DELETE_MOVIE, payload:id});
-}
+export const deleteMovie = (id) => {
+  return { type: DELETE_MOVIE, payload: id };
+};
+
+export const addMovie = (movie) => {
+  return {
+    type: ADD_MOVIE,
+    payload: movie,
+  };
+};
+
+export const toggleFavorite = (favorite) => {
+  return { type: TOGGLE_FAVORITE, payload: favorite };
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import reducer from "./favoritesReducer";
+import movieReducer from "./movieReducer";
 
-import movieReducer from './movieReducer';
+export const reducerCombine = combineReducers({
+  movie: movieReducer,
+  favorite: reducer,
+});
 
 export default movieReducer;
